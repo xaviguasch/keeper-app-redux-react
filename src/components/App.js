fbb786cec12a0704976dae6d15cd7b900a13a9d6
xaviguasch <xaviguasch@gmail.com>
2020-01-06 22:28:06 +0100

    added note creation logic

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 import Footer from './Footer'
 import Note from './Note'
 import CreateArea from './CreateArea'
 
 function App() {
+    const [notes, setNotes] = useState([])
+
     return (
         <div>
             <Header />
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,16 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function CreateArea() {
+function CreateArea({ onAdd }) {
+    const [note, setNote] = useState({
+        title: '',
+        content: ''
+    })
+
+    function handleChange(event) {
+        const origin = event.target.name
+        const newValue = event.target.value
+
+        if (origin === 'title') {
+            setNote(prevNote => {
+                return {
+                    title: newValue,
+                    content: prevNote.content
+                }
+            })
+        } else if (origin === 'content') {
+            setNote(prevNote => {
+                return {
+                    title: prevNote.title,
+                    content: newValue
+                }
+            })
+        }
+    }
+
     return (
         <div>
             <form>
-                <input name='title' placeholder='Title' />
+                <input
+                    onChange={handleChange}
+                    name='title'
+                    placeholder='Title'
+                    value={note.title}
+                />
                 <textarea
+                    onChange={handleChange}
                     name='content'
                     placeholder='Take a note...'
                     rows='3'
+                    value={note.content}
                 />
-                <button>Add</button>
+                <button
+                // onClick={() => {
+                //     onAdd(inputText)
+                //     setInputTitle('')
+                //     setInputText('')
+                // }}
+                >
+                    Add
+                </button>
             </form>
         </div>
     )
