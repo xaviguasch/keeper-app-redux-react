ee143938d883beb9978ec9e5bce3b458e299ce50
xaviguasch <xaviguasch@gmail.com>
2020-01-07 19:10:00 +0100

    added conditional rendering to the note title

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react'
 import AddIcon from '@material-ui/icons/Add'
 import Fab from '@material-ui/core/Fab'
+import Zoom from '@material-ui/core/Zoom'
 
 function CreateArea(props) {
+    const [isExpanded, setExpanded] = useState(false)
+
     const [note, setNote] = useState({
         title: '',
         content: ''
@@ -28,25 +31,35 @@ function CreateArea(props) {
         event.preventDefault()
     }
 
+    function expand() {
+        setExpanded(true)
+    }
+
     return (
         <div>
             <form className='create-note'>
-                <input
-                    name='title'
-                    onChange={handleChange}
-                    value={note.title}
-                    placeholder='Title'
-                />
+                {isExpanded && (
+                    <input
+                        name='title'
+                        onChange={handleChange}
+                        value={note.title}
+                        placeholder='Title'
+                    />
+                )}
+
                 <textarea
                     name='content'
+                    onClick={expand}
                     onChange={handleChange}
                     value={note.content}
                     placeholder='Take a note...'
                     rows='3'
                 />
-                <Fab onClick={submitNote}>
-                    <AddIcon />
-                </Fab>
+                <Zoom in={true}>
+                    <Fab onClick={submitNote}>
+                        <AddIcon />
+                    </Fab>
+                </Zoom>
             </form>
         </div>
     )
